b9201159689b4fdf2f5639cef73becc441069c45
Ninad Akolekar <ninad.akolekar@gmail.com>
2018-03-04 23:43:33 +0530

    Updated message structure

--- src/message/message.go
+++ src/message/message.go
@@ -14,7 +14,24 @@ type NewClientMessage struct {
 
 //UpdateMessage ... Structure: Gameplay Update Message
 type UpdateMessage struct {
-	ID       uint32 `json:"id"`       // 1
-	Message  string `json:"message"`  // 2
-	BoxColor string `json:"boxColor"` // Color of the box
+	ID      uint32 `json:"id"`      // 1
+	Message string `json:"message"` // 2
+
+	TankX       uint32  `json:"tank_x"`
+	TankY       uint32  `json:"tank_y"`
+	RotationInc float32 `json:"rotationIcrement"`
+
+	BulletCount uint32 `json:"bulletCount"`
+	BulletX     uint32 `json:"bullet_x"`
+	BulletY     uint32 `json:"bullet_y"`
+
+	OtherX uint32 `json:"other_x"`
+	OtherY uint32 `json:"other_y"`
+
+	CenterX uint32 `json:"center_x"`
+	CenterY uint32 `json:"center_y"`
+
+	Health uint32 `json:"health"`
+	Exit   bool   `json:"exit"`
+	Won    bool   `json:"won"`
 }
